8b97318515e41ffc62090880d374eea85db3a634
Robert Keen <robertkeen9@gmail.com>
2020-06-14 10:20:41 -0700

    removed irrelevant comment

--- volume_up.sh
+++ volume_up.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Called by DWM when the volume up key is pressed
-# only adjusts volume for bluetooth
 source /home/robert/scripts/VARIABLES
 pamixer --sink $SINK -i 2
 /home/robert/scripts/refresh_bar.sh
